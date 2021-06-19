f689616fdacc69b33fa9bbfb9977daabdf155209
yanjun0501 <i@yanjun-forever.cn>
2021-06-19 19:30:41 +0800

    feat: swap

--- src/views/Bridge.vue
+++ src/views/Bridge.vue
@@ -3,32 +3,76 @@
     <div class="bridgeTitle">Bridge</div>
     <div class="bridgeMain">
       <div class="formlabel">Choose Assets</div>
-      <div class="select">
+      <div class="select" @click="showSelectModal">
         <div class="bType">
           <img :src="require('../assets/usdt_48.png')" />
           USDT
         </div>
         <div class="selectIcon"></div>
+        <div class="selectModal" v-if="isShowSelectModal">
+          <div class="selectModalTitle">Select a token</div>
+          <div class="selectModalFlex">
+            <div>USDT</div>
+            <div>DOT</div>
+            <div>ETH</div>
+          </div>
+          <input class="selectModalInput" />
+          <div class="selectModalList"></div>
+        </div>
       </div>
       <div class="swap">
         <div class="swapLeft">
           <div class="formlabel">From</div>
-          <div class="swapItem" style="margin-right: 20px">
-            <div>
-              <img src="" />
+          <div
+            class="swapItem"
+            style="margin-right: 20px"
+            @click="showSwapModal1"
+          >
+            <div class="bType">
+              <img :src="require('../assets/usdt_48.png')" />
+              USDT
             </div>
-            <div></div>
+            <div class="selectIcon"></div>
+            <div class="swapSelectModal" v-if="isShowSwapModal1">
+              <div
+                class="swapSelectModalItem"
+                v-for="(item, index) in chainList"
+                :key="index"
+              >
+                <div><img :src="item.img" />{{ item.name }}</div>
+                <img class="bs" :src="require('../assets/bs.png')" />
+              </div>
+            </div>
           </div>
         </div>
         <div class="swapIcon"></div>
         <div class="swapRight">
           <div class="formlabel">To</div>
-          <div class="swapItem"></div>
+          <div class="swapItem" @click="showSwapModal2">
+            <div class="bType">
+              <img :src="require('../assets/usdt_48.png')" />
+              USDT
+            </div>
+            <div class="selectIcon"></div>
+            <div class="swapSelectModal" v-if="isShowSwapModal2">
+              <div
+                class="swapSelectModalItem"
+                v-for="(item, index) in chainList"
+                :key="index"
+              >
+                <div><img :src="item.img" />{{ item.name }}</div>
+                <img class="bs" :src="require('../assets/bs.png')" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="formlabel">Destination Address</div>
       <input type="text" style="margin-bottom: 30px" />
-      <div class="formlabel">Amount</div>
+      <div class="formlabel">
+        <div>Amount</div>
+        <div>Balance: 141231231 USDT</div>
+      </div>
       <input type="text" style="margin-bottom: 20px" />
       <div class="notice">
         <div class="noticeLeft">Reminder:</div>
@@ -37,20 +81,28 @@
           <div class="noticeBottom">2.The maximum amount is 10000 USDT</div>
         </div>
       </div>
-      <div class="swapBtn">Confirm</div>
+      <div class="swapBtn" @click="showModal">Confirm</div>
     </div>
+    <Modal v-model="isShowModal" />
   </div>
 </template>
 
 <script>
+import Modal from './Modal.vue';
+
 const eth = require('../assets/eth.png');
 const polk = require('../assets/dot.png');
 const heco = require('../assets/ht.png');
 const bsc = require('../assets/bnb.png');
 
 export default {
+  components: { Modal },
   data() {
     return {
+      isShowSelectModal: false,
+      isShowSwapModal1: false,
+      isShowSwapModal2: false,
+      isShowModal: false,
       chainList: [
         {
           name: 'Ethereum',
@@ -70,6 +122,20 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    showModal() {
+      this.isShowModal = true;
+    },
+    showSelectModal() {
+      this.isShowSelectModal = !this.isShowSelectModal;
+    },
+    showSwapModal1() {
+      this.isShowSwapModal1 = !this.isShowSwapModal1;
+    },
+    showSwapModal2() {
+      this.isShowSwapModal2 = !this.isShowSwapModal2;
+    },
   },
 };
 </script>
@@ -101,8 +167,11 @@ export default {
     font-size: 16px;
     color: #000000;
     margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
   }
   .select {
+    z-index: 1;
     margin-bottom: 30px;
     width: 580px;
     height: 80px;
@@ -112,26 +181,65 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    .bType {
-      font-weight: bold;
-      font-size: 20px;
-      color: #000000;
-      display: flex;
-      align-items: center;
-      margin-left: 30px;
-      img {
-        margin-right: 12px;
-        width: 48px;
-        height: 48px;
+    position: relative;
+    .selectModal {
+      z-index: 2;
+      position: absolute;
+      width: 580px;
+      height: 353px;
+      top: 90px;
+      background: #ffffff;
+      border: 1px solid rgba(0, 0, 0, 0.1);
+      box-sizing: border-box;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+      border-radius: 10px;
+      padding: 30px;
+      box-sizing: border-box;
+      .selectModalTitle {
+        font-size: 16px;
+        color: #000000;
+        text-align: left;
+        margin-bottom: 20px;
+      }
+      .selectModalFlex {
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+        div {
+          margin-right: 10px;
+          width: 95px;
+          height: 40px;
+          background: rgba(0, 0, 0, 0.1);
+          border-radius: 10px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-weight: bold;
+          font-size: 20px;
+          color: #000000;
+        }
       }
     }
-    .selectIcon {
-      margin-right: 35px;
-      width: 20px;
-      height: 10px;
-      background: url("../assets/select_open.png") no-repeat;
-      background-size: 20px 10px;
+  }
+  .bType {
+    font-weight: bold;
+    font-size: 20px;
+    color: #000000;
+    display: flex;
+    align-items: center;
+    margin-left: 30px;
+    img {
+      margin-right: 12px;
+      width: 48px;
+      height: 48px;
     }
+  }
+  .selectIcon {
+    margin-right: 35px;
+    width: 20px;
+    height: 10px;
+    background: url("../assets/select_open.png") no-repeat;
+    background-size: 20px 10px;
   }
   .swap {
     margin-bottom: 30px;
@@ -139,6 +247,7 @@ export default {
     align-items: center;
     position: relative;
     .swapIcon {
+      // z-index: 1;
       width: 48px;
       height: 48px;
       background: url("../assets/swap_icon.png") no-repeat;
@@ -154,6 +263,43 @@ export default {
       border: 1px solid #e7e7e7;
       box-sizing: border-box;
       border-radius: 10px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: relative;
+      .swapSelectModal {
+        position: absolute;
+        bottom: -300px;
+        width: 280px;
+        height: 289px;
+        background: #ffffff;
+        border: 1px solid #e7e7e7;
+        box-sizing: border-box;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+        border-radius: 10px;
+        .swapSelectModalItem {
+          width: 279px;
+          height: 74px;
+          background: #f6f6f6;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          div {
+            &:nth-of-type(1) {
+              margin-left: 30px;
+              display: flex;
+              align-items: center;
+              font-size: 16px;
+              color: #000000;
+              img {
+                width: 36px;
+                height: 36px;
+                margin-right: 20px;
+              }
+            }
+          }
+        }
+      }
     }
   }
   .notice {
@@ -193,5 +339,19 @@ input {
   border: 1px solid #e7e7e7;
   box-sizing: border-box;
   border-radius: 10px;
+}
+.selectModalInput {
+  margin-bottom: 20px;
+  width: 520px;
+  height: 80px;
+  background: rgba(169, 169, 169, 0.1);
+  border: 1px solid #e7e7e7;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+.bs {
+  width: 17px;
+  height: 17px;
+  margin-right: 30px;
 }
 </style>
---
+++ src/views/Modal.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="modal" v-if="value">
+    <div class="modalWrap">
+      <div class="modalWrapTitle">
+        <div>Confirm</div>
+        <div @click="closeModal"></div>
+      </div>
+      <div class="modalWrapSwap">
+        <img :src="require('../assets/dot.png')" />
+        <div></div>
+        <img :src="require('../assets/eth.png')" />
+      </div>
+      <div class="modalWrapText">1000.00 USDT</div>
+      <div class="modalWrapMain">
+        <div class="line"></div>
+        <div class="des">
+          <div>Asset:</div>
+          <div>
+            <img src="" alt="" />
+            USDT
+          </div>
+        </div>
+        <div class="des">
+          <div>Destination:</div>
+          <div>0xa7f6ac......B153531D</div>
+        </div>
+        <div class="des">
+          <div>Network Fee:</div>
+          <div>8.00USDT</div>
+        </div>
+        <div class="des">
+          <div>You will receive:</div>
+          <div>≈992.00USDT</div>
+        </div>
+        <div class="line"></div>
+      </div>
+      <div class="modalWrapNotice">
+        1.Please initiate a single Transfer,we will only monitor the first
+        transfer. ...
+      </div>
+      <div>
+        <el-checkbox v-model="checked"
+          >I have read and agree to the Terms of use.</el-checkbox
+        >
+      </div>
+      <div class="modalWrapBtn">Confirm</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    closeModal() {
+      this.$emit('input', false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .modalWrap {
+    width: 480px;
+    height: 633px;
+    left: 720px;
+    top: 223px;
+    background: #ffffff;
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding: 30px;
+    .modalWrapTitle {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      div {
+        &:nth-of-type(1) {
+          font-weight: bold;
+          font-size: 30px;
+          color: #1e2226;
+        }
+        &:nth-of-type(2) {
+          width: 20px;
+          height: 20px;
+          background: url("../assets/close.png") no-repeat;
+          background-size: 20px 20px;
+        }
+      }
+    }
+    .modalWrapSwap {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 48px;
+        height: 48px;
+      }
+      div {
+        margin: 0 16px;
+        width: 17px;
+        height: 20px;
+        background: url("../assets/step.png") no-repeat;
+        background-size: 17px 20px;
+      }
+    }
+    .modalWrapText {
+      font-weight: bold;
+      font-size: 20px;
+      display: flex;
+      justify-content: center;
+      color: #1e2226;
+      margin: 20px 0 30px;
+    }
+    .modalWrapMain {
+      .line {
+        width: 420px;
+        height: 1px;
+        background: url("../assets/line.png") no-repeat;
+        background-size: 420px 1px;
+        margin-bottom: 22px;
+      }
+      .des {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        div {
+          margin-bottom: 16px;
+          &:nth-of-type(1) {
+            font-size: 16px;
+            color: #1e2226;
+          }
+          &:nth-of-type(2) {
+            font-weight: bold;
+            font-size: 16px;
+            color: #020202;
+          }
+        }
+      }
+    }
+    .modalWrapNotice {
+      font-size: 16px;
+      color: rgba(30, 34, 38, 0.4);
+      text-align: left;
+      margin-bottom: 20px;
+    }
+    .modalWrapBtn {
+      margin-top: 24px;
+      width: 420px;
+      height: 60px;
+      background: rgba(51, 51, 51, 0.4);
+      border-radius: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+      font-size: 16px;
+      color: #ffffff;
+    }
+  }
+}
+</style>
